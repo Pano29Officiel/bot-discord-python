7c80035e4b33ba941e6cc9068264c9756e9d3825
Your Name <leo.lebigot06@gmail.com>
2021-06-22 10:55:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Créer un BOT DISCORD en PYTHON
+Bienvenue sur le repo officiel de la série de vidéos "Crée un bot discord avec python" réalisée par Pano29Officiel.
+
+[![Alt text](https://cdn.discordapp.com/attachments/775770838309404722/856615611094663188/baniere_parent.png)](https://www.pano29officiel.tk)
+
+Cette série de vidéos vous permet d'apprendre à développer un BOT DISCORD avec PYTHON. Tous les scripts du projet sont en C# et il est recommandé de suivre la série en version 2019.4.
+
+Le contenu du projet pour chaque épisode est disponible [ICI](https://github.com/Pano29Officiel/branches), choisissez la branche de votre choix, téléchargez là et ouvrez le projet sur votre éditeur de code.
+
+► Si vous suivez cette série et/ou utilisez ce repo GitHub pensez à vous abonner a ma [chaine](https://bitly.com/Pano29YTB) ! Ce n'est pas obligatoire mais c'est fortement apprécié !
+
+
---
+++ episodes/episode-1.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+
+from PIL import Image
+from io import BytesIO
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="=", intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready.")
+
+
+
+
+
+@bot.command()
+async def wanted(ctx, user: discord.Member = None):
+    if user == None:
+        user = ctx.author
+
+    wanted = Image.open("wanted.png")
+
+    asset = user.avatar_url_as(size = 128)
+
+    data = BytesIO(await asset.read())
+
+    pfp = Image.open(data)
+
+    pfp = pfp.resize((177,177))
+
+    wanted.paste(pfp, (50,50))
+
+    wanted.save("profile.png")
+
+    await ctx.send(file =  discord.File("profile.png"))
+
+
+
+
+
+
+
+@bot.group(pass_context=True)
+async def Help(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Invalid sub command passed...')
+
+@Help.command(pass_context=True)
+async def Ui(ctx):
+    msg = 'Finally got success {0.author.mention}'.format(ctx.message)
+    await ctx.send(msg)
+
+
+@bot.command()
+async def ping(ctx):
+    embed = discord.Embed(title="** :ping_pong:  •  Pong !**", description=f"`{round(bot.latency * 1000)}ms`",
+                          color=0x3bc200)
+    await ctx.send(embed=embed)
+
+
+bot.run("ODU1ODMzODY5OTAzNDYyNDIw.YM4PZw.0c0HM_dE04S34EjMKsD0zSxbiAw")
